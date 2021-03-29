b799167234d88ce9b2e0644a3b5829a1d8d558c4
Charlesjahn <72029865+Charlesjahn@users.noreply.github.com>
2021-03-29 20:01:21 +0100

    ajusts
    adjusts on CSS
    adjust on Table
    implementing JS for menu

---
+++ menu.js
@@ -0,0 +1,81 @@
+var ciclo = 0;
+
+function add1(){
+    let ciclo = ciclo + 1;
+    let valor = parseFloat(document.getElementById("p1").value.replace(",","."));
+    let total = valor;
+    alert(valor);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function add2(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("2");
+    
+}
+function add3(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("3");
+    
+}
+function add4(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("4");
+    
+}
+function add5(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("5");
+    
+}
+function add6(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("6");
+    
+}
+function add7(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("7");
+    
+}
+function add8(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("8");
+    
+}
+function add9(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("9");
+    
+}
+function add10(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("10");
+    
+}
+function add11(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("11");
+    
+}
+function add12(){
+    var ciclo  = ciclo + 1;
+    var valor = document.getElementById("12");
+    
+}
