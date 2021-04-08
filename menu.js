95fb1af7d61d6247d7bf1f4cdf492f1c53bbf6b5
Charlesjahn <72029865+Charlesjahn@users.noreply.github.com>
2021-04-08 11:57:45 +0100

    adjusts
    small adjust
    in some lines are missing " ; "
    and make table be create on load, not on click

--- menu.js
+++ menu.js
@@ -1,19 +1,18 @@
-function tableCreate() {
+(function() {
 
     //Array for topic of options
-    let tipo = ["STARTERS","MAIN","DESERTS","DRINK"]
+    let tipo = ["STARTERS","MAIN","DESERTS","DRINK"];
+    
     //Array for list of option
-    let options =["Mixed Salads","Coked eggs","French fries/Chips","A la Minute","Chicken Risotto","Stroganoff Of Soy","Fruit Salad","Sweet Pudim","Sweet Rise","Hot Drinks (hot chocolate, coffe & tea)","Soft Drinks (Coke,Fanta,7up & Sprite)","Glass of wine (Pinot, Merlot & Cabernet)"]
+    let options =["Mixed Salads","Coked eggs","French fries/Chips","A la Minute","Chicken Risotto","Stroganoff Of Soy","Fruit Salad","Sweet Pudim","Sweet Rise","Hot Drinks (hot chocolate, coffe & tea)","Soft Drinks (Coke,Fanta,7up & Sprite)","Glass of wine (Pinot, Merlot & Cabernet)"];
+
     //Array for prices
-    let price = ["2,00","0,30","3,50","10,30","8,50","12,00","3,50","4,00","3,00","3,50","2,50","5,50",]
+    let price = ["2,00","0,30","3,50","10,30","8,50","12,00","3,50","4,00","3,00","3,50","2,50","5,50",];
 
     var buttonAdd = document.createElement("buttom");
     var div = document.getElementsByTagName('div')[0];
     var tbl = document.createElement('table');
     var tbdy = document.createElement('tbody');
-    
-    tbl.setAttribute('border', '0');
-    
 
     var ciclo = 0;
     for(var x = 0; x < 4; x++){
@@ -24,7 +23,6 @@ function tableCreate() {
               i == 0 && j == 1 ? td.setAttribute('colSpan', '7') : null;
               td.setAttribute('class','optionTitle');
               td.appendChild(document.createTextNode(tipo[x]))
-              
             break
           } else{
             switch(j) {
@@ -32,7 +30,9 @@ function tableCreate() {
                 var button = document.createElement('button');
                 button.setAttribute('type','button');
                 button.setAttribute('class','button');
+
                 button.onclick = function() {add();}
+                
                 j == 1 ? button.appendChild(document.createTextNode('ADD')): null;
                 j == 1 ? td.appendChild(button): null;
                 ciclo++;
@@ -49,11 +49,8 @@ function tableCreate() {
               case 6:
                 j == 6 ? td.appendChild(document.createTextNode(price[ciclo-1])): null;
                 j == 6 ? td.setAttribute('id','price'): null;
-              
                 break;
               default:
-                // code block
-                
             }
             var td = document.createElement('td');
             tr.appendChild(td)
@@ -63,11 +60,11 @@ function tableCreate() {
       }
     }
     tbl.appendChild(tbdy);
-    div.appendChild(tbl)
-  }tableCreate();
-
+    div.appendChild(tbl);
+  })();
+  
   function add(){
-    var option = getElementsById("price");
-    alert(option);
+    let option = document.getElementsById("price");
+    alert("Item added");
   } ;
   
