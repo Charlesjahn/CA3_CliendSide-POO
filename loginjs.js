30b82ae9d74c37780365e12c55244e26d8af7ded
Charlesjahn <72029865+Charlesjahn@users.noreply.github.com>
2021-04-20 19:44:31 +0100

    Update validate password
    changing of validate password , and implementing regexp.

--- loginjs.js
+++ loginjs.js
@@ -1,18 +1,24 @@
-function validate()
-{    /**Get staff`s name and password, to valid the user*/
-    var staffname = document.getElementById("staffname").value;
-    var password = document.getElementById("password").value;
-    
-    /**It will change the direction of the page, in case name and password is right*/
-    if(staffname == "admin" && password == "user"){
-        alert("login succesfully");
+
+function validate(){
+
+  let password = document.getElementById("password");
+  let isValid = validatePassword(password);
+  if(isValid){
+    alert("login succesfully");
         
-        document.logPass.action = "menu.html"; 
-        document.logPass.submit();
-    }
-    /**It will remain in the login page, in sace name and password were wrong */
-    else{
-        alert("login failed");
-        return false;
-    }
+    document.logPass.action = "./menu.html"; 
+    document.logPass.submit();
+  }
+  else{
+    document.getElementById("error").innerHTML = "Error";
+  }
+}
+
+function validatePassword(passTest){
+
+  var passworkRegexp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+  let result = passworkRegexp.test(passTest.value);
+  return result;
+
 }
