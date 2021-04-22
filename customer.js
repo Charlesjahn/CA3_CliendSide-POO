e11b88e0dc530e38091434fa47d31fccf101ebb5
Charlesjahn <72029865+Charlesjahn@users.noreply.github.com>
2021-04-22 09:05:41 +0100

    update, json
    implementing json, small changes com menu html, js, and css

---
+++ customer.js
@@ -0,0 +1,19 @@
+async function getJSON(){
+    //getting the information from the website
+    const response = await fetch(
+        "https://randomuser.me/api/?results=5"
+    ).catch((err) => console.log(err));
+
+    //transforming the response in JSON.
+    const json = await response.json().catch((err) => console.log(err));
+
+    //
+    let customers = [];
+
+    for(var i = 0; i< json.results.length; i++){
+       // creating the customers
+        customers[i] = json.results[i];
+
+    }
+    return customers;
+}
--- menu.js
+++ menu.js
@@ -1,69 +1,85 @@
-(function() {
+//Array for topic of options
+var tipo = ["STARTERS", "MAIN", "DESERTS", "DRINK"];
 
-    //Array for topic of options
-    let tipo = ["STARTERS","MAIN","DESERTS","DRINK"];
-    
-    //Array for list of option
-    let options =["Mixed Salads","Coked eggs","French fries/Chips","A la Minute","Chicken Risotto","Stroganoff Of Soy","Fruit Salad","Sweet Pudim","Sweet Rise","Hot Drinks (hot chocolate, coffe & tea)","Soft Drinks (Coke,Fanta,7up & Sprite)","Glass of wine (Pinot, Merlot & Cabernet)"];
+//Array for list of option
+var options = ["Mixed Salads", "Coked eggs", "French fries/Chips", "A la Minute", "Chicken Risotto", "Stroganoff Of Soy", "Fruit Salad", "Sweet Pudim", "Sweet Rise", "Hot Drinks (hot chocolate, coffe & tea)", "Soft Drinks (Coke,Fanta,7up & Sprite)", "Glass of wine (Pinot, Merlot & Cabernet)"];
 
-    //Array for prices
-    let price = ["2,00","0,30","3,50","10,30","8,50","12,00","3,50","4,00","3,00","3,50","2,50","5,50",];
+//Array for prices
+var price = ["2,00", "0,30", "3,50", "10,30", "8,50", "12,00", "3,50", "4,00", "3,00", "3,50", "2,50", "5,50",];
 
-    var buttonAdd = document.createElement("buttom");
-    var div = document.getElementsByTagName('div')[0];
-    var tbl = document.createElement('table');
-    var tbdy = document.createElement('tbody');
+var sum = 0;
 
-    var ciclo = 0;
-    for(var x = 0; x < 4; x++){
-      for (var i = 0; i < 4; i++) {
-        var tr = document.createElement('tr');
-        for (var j = 0; j < 7; j++) {
-          if (i == 0 && j == 1 ) {
-              i == 0 && j == 1 ? td.setAttribute('colSpan', '7') : null;
-              td.setAttribute('class','optionTitle');
-              td.appendChild(document.createTextNode(tipo[x]))
-            break
-          } else{
-            switch(j) {
-              case 1:
-                var button = document.createElement('button');
-                button.setAttribute('type','button');
-                button.setAttribute('class','button');
+(function () {
+  var buttonAdd = document.createElement("buttom");
+  var div = document.getElementsByTagName('div')[0];
+  var tbl = document.createElement('table');
+  var tbdy = document.createElement('tbody');
 
-                button.onclick = function(){add();}
+  var ciclo = 0;
+  let buttoms = [];
 
-                j == 1 ? button.appendChild(document.createTextNode('ADD')): null;
-                j == 1 ? td.appendChild(button): null;
-                ciclo++;
-                
-              case 2:
-                j == 2 ? td.appendChild(document.createTextNode(ciclo +":")): null;
-                j == 2 ? td.setAttribute('class','optionNum'): null;
-                
-              case 3:
-                j == 3 ? td.appendChild(document.createTextNode(options[ciclo-1])): null;
-                j == 3 ? td.setAttribute('class','optionDish'): null;
-              case 5:
-                j == 5 ? td.appendChild(document.createTextNode("€")): null;
-              case 6:
-                j == 6 ? td.appendChild(document.createTextNode(price[ciclo-1])): null;
-                j == 6 ? td.setAttribute('id','price'): null;
-                break;
-              default:
-            }
-            var td = document.createElement('td');
-            tr.appendChild(td)
+  for (var x = 0; x < 4; x++) {
+
+    for (var i = 0; i < 4; i++) {
+      var tr = document.createElement('tr');
+
+      for (var j = 0; j < 7; j++) {
+        console.log(ciclo);
+
+        if (i == 0 && j == 1) {
+          i == 0 && j == 1 ? td.setAttribute('colSpan', '7') : null;
+          td.setAttribute('class', 'optionTitle');
+          td.appendChild(document.createTextNode(tipo[x]))
+          break
+
+        } else {
+          switch (j) {
+            case 1:
+              buttoms[ciclo] = document.createElement('button');
+              buttoms[ciclo].setAttribute('type', 'button');
+              buttoms[ciclo].setAttribute('class', 'button');
+              buttoms[ciclo].setAttribute('id', 'button' + (ciclo+1));
+              buttoms[ciclo].onclick = (function (ciclo) { return function() {add(ciclo);} }) (ciclo);
+
+              j == 1 ? buttoms[ciclo].appendChild(document.createTextNode('ADD')) : null;
+              j == 1 ? td.appendChild(buttoms[ciclo]) : null;
+              ciclo++;
+              break;
+
+            case 2:
+              j == 2 ? td.appendChild(document.createTextNode(ciclo + ":")) : null;
+              j == 2 ? td.setAttribute('class', 'optionNum') : null;
+              break;
+
+            case 3:
+              j == 3 ? td.appendChild(document.createTextNode(options[ciclo - 1])) : null;
+              j == 3 ? td.setAttribute('class', 'optionDish') : null;
+              break;
+
+            case 5:
+              j == 5 ? td.appendChild(document.createTextNode("€")) : null;
+              break;
+
+            case 6:
+              j == 6 ? td.appendChild(document.createTextNode(price[ciclo - 1])) : null;
+              j == 6 ? td.setAttribute('id', 'price' + ciclo) : null;
+              break;
           }
+          var td = document.createElement('td');
+          tr.appendChild(td)
         }
-        tbdy.appendChild(tr);
       }
+      tbdy.appendChild(tr);
     }
-    tbl.appendChild(tbdy);
-    div.appendChild(tbl);
-  })();
+  }
+  tbl.appendChild(tbdy);
+  div.appendChild(tbl);
+})();
+
+function add(i) {
   
-  function add(){
-    alert("Item added");
-  } ;
-  
+  sum += parseFloat(price[i]);
+
+  alert(sum);
+
+};
